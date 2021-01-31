df5d622b2d637a69c96be4946b12ebfc13d21cee
Dimastii <cveeta@un-i4.kzn.21-school.ru>
2021-01-31 21:28:14 +0300

    спрайт еать но он стрёмный

--- main.c
+++ main.c
@@ -5,8 +5,8 @@
 #include "gnl/get_next_line.h"
 
 //#define PI 3.14159265359
-#define ANLGLE M_PI / 5
-#define N_REY vars->size_win_w
+#define ANLGLe M_PI / 5
+#define N_ReY vars->size_win_w
 
 
 #define mapWidth 24
@@ -18,25 +18,25 @@ int worldMap[mapWidth][mapHeight]=
 				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
 				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
 				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-				{1,0,0,0,0,0,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-				{1,0,0,0,0,0,0,0,5,0,0,5,5,0,5,5,0,0,0,7,0,7,0,1},
-				{1,0,0,0,0,0,0,5,0,0,0,5,0,0,0,5,0,0,0,0,0,7,0,1},
-				{1,0,0,0,0,0,0,5,0,0,0,0,0,0,0,5,0,0,0,0,0,0,0,1},
-				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,7,0,1},
-				{1,0,0,0,0,0,0,5,0,0,0,0,0,0,0,5,0,0,0,0,0,0,0,1},
-				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,7,0,1},
-				{1,0,0,0,0,0,0,5,5,0,0,0,5,0,5,0,0,0,7,7,0,7,0,1},
-				{1,6,0,6,0,0,0,6,6,0,0,0,6,0,6,6,0,0,6,6,0,6,0,1},
 				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-				{1,0,0,6,0,0,0,6,6,0,0,0,6,0,6,6,0,0,6,6,0,6,0,1},
-				{1,0,0,4,0,0,0,4,4,0,0,0,6,0,2,2,0,0,2,2,0,3,0,1},
-				{1,0,0,0,0,0,0,0,0,0,0,0,6,0,0,0,0,0,0,2,0,0,0,1},
-				{1,0,0,0,0,0,0,0,0,0,0,0,6,0,0,0,0,0,0,2,0,0,0,1},
-				{1,0,0,0,0,0,0,0,0,0,0,0,6,0,0,0,0,0,0,2,0,0,0,1},
+				{1,0,0,0,0,0,0,9,1,0,0,1,1,0,1,1,0,0,0,7,0,7,0,1},
+				{1,0,0,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0,0,7,0,1},
+				{1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,7,0,1},
+				{1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,7,0,1},
+				{1,0,0,0,0,0,0,1,1,0,0,0,1,0,1,0,0,0,7,7,0,7,0,1},
+				{1,6,0,1,0,0,0,1,1,0,0,0,1,0,1,1,0,0,1,1,0,1,0,1},
 				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-				{1,0,0,5,0,0,0,0,0,0,0,0,6,0,0,0,0,0,0,2,0,0,0,1},
-				{1,0,0,0,0,0,0,0,0,0,0,0,6,0,0,0,0,0,0,2,0,0,0,1},
-				{1,0,0,0,0,0,0,0,0,0,0,0,6,0,0,0,0,0,0,2,0,0,0,1},
+				{1,0,0,1,0,0,0,1,1,0,0,0,1,0,1,1,0,0,1,1,0,1,0,1},
+				{1,0,0,4,0,0,0,4,4,0,0,0,1,0,1,1,0,0,1,1,0,3,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,5,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,1},
 				{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
 		};
 //		{
@@ -142,29 +142,29 @@ int             key_hook(int keycode, t_vars *vars)
 	}
 }
 
-void	write_map(t_vars* vars)
-{
-	int i = mapWidth;
-	int j = mapHeight;
-	int sc1 = vars->size_win_w / mapWidth;
-	int sc2 = vars->size_win_w / mapHeight;
-	while (i--)
-	{
-		j = mapHeight;
-		while (j--) {
-			if (worldMap[j][i] == 0) {
-				sc1 = vars->size_win_w / mapWidth;
-				while (sc1-- != 0) {
-					sc2 = vars->size_win_w / mapHeight;
-					while (sc2-- != 0)
-					my_mlx_pixel_put(&vars->img, (int) (j * (vars->size_win_h / mapHeight) + sc2),
-									 (int) (i * (vars->size_win_w / mapWidth) + sc1), 0x8800FF00);
-					//my_mlx_pixel_put(&vars->img, (int) (j * (vars->size_win_h / mapHeight) + sc),(int) (i * (vars->size_win_w / mapWidth) + sc), 0x8800FF00);
-				}
-			}
-		}
-	}
-}
+//void	write_map(t_vars* vars)
+//{
+//	int i = mapWidth;
+//	int j = mapHeight;
+//	int sc1 = vars->size_win_w / mapWidth;
+//	int sc2 = vars->size_win_w / mapHeight;
+//	while (i--)
+//	{
+//		j = mapHeight;
+//		while (j--) {
+//			if (worldMap[j][i] == 0) {
+//				sc1 = vars->size_win_w / mapWidth;
+//				while (sc1-- != 0) {
+//					sc2 = vars->size_win_w / mapHeight;
+//					while (sc2-- != 0)
+//					my_mlx_pixel_put(&vars->img, (int) (j * (vars->size_win_h / mapHeight) + sc2),
+//									 (int) (i * (vars->size_win_w / mapWidth) + sc1), 0x8800FF00);
+//					//my_mlx_pixel_put(&vars->img, (int) (j * (vars->size_win_h / mapHeight) + sc),(int) (i * (vars->size_win_w / mapWidth) + sc), 0x8800FF00);
+//				}
+//			}
+//		}
+//	}
+//}
 
 void ft_round(double min_angle,double *cx, double *cy,t_vars *vars)
 {
@@ -196,9 +196,6 @@ void ft_round(double min_angle,double *cx, double *cy,t_vars *vars)
 		vars->sprite->offset_mode_cord_sprite_x = 0;
 		vars->sprite->offset_mode_cord_sprite_y = 1;
 	}
-	else
-		printf("11111111111\n");
-
 }
 
 void	ft_render(t_vars *vars, double len_r, double min_angle, int num)
@@ -208,49 +205,59 @@ void	ft_render(t_vars *vars, double len_r, double min_angle, int num)
 	double real_len = len_r * fabs(sin(vars->angle_p + M_PI/2 - min_angle));
 	double wall_h = vars->size_win_h / real_len;
 	int drawStart = -wall_h / 2 + vars->size_win_h / 2;
-	int drawEnd = wall_h / 2 + vars->size_win_h / 2;
+	int drawend = wall_h / 2 + vars->size_win_h / 2;
 	if (drawStart <0) drawStart = 0;
-	if (drawEnd >= vars->size_win_h) drawEnd = vars->size_win_h - 1;
+	if (drawend >= vars->size_win_h) drawend = vars->size_win_h - 1;
 
 
 	wall_y = drawStart;
 
-	while (wall_y < drawEnd)
+	while (wall_y < drawend)
 	{
 		my_mlx_pixel_put(&vars->img, num - 1 ,wall_y, vars->color);
 		wall_y++;
 	}
 }
 
-void	ft_render_sprite(double len_to_sprt, double lenx, double x, double y, t_vars *vars)
+void	ft_render_sprite(double len_to_sprt, double lenx, double x, double y, t_vars *vars , int num_rey)
 {
 	double xs;
 	double ys;
 	int i;
 	double len_sp;
+	double wall_y;
 
 	len_sp = (sqrt(2) - 1) / 2;
 	i = 0;
-
-
+	wall_y = vars->size_win_h / 2;
 	while (vars->sprite[i].create != -1)
 	{
 		xs = fabs((vars->sprite[i].enter_on_lenx_x - vars->sprite->enter_on_lenx_y ) / 2);
 		ys = fabs((vars->sprite->enter_on_leny_x - vars->sprite->enter_on_leny_y ) / 2);
 		vars->sprite[i].len_sprt -= sqrt(pow(xs - vars->sprite[i].x_pred, 2) + pow(ys - vars->sprite[i].y_pred, 2));
+		//printf(" = %d \t %d  %f\n",0, vars->sprite[0].create, num);
 
+		double wall_h = vars->size_win_h / len_to_sprt;
+		int drawStart = -wall_h / 2 + vars->size_win_h / 2;
+		int drawend = wall_h / 2 + vars->size_win_h / 2;
+		if (drawStart <0) drawStart = 0;
+		if (drawend >= vars->size_win_h) drawend = vars->size_win_h - 1;
+
+
+		wall_y = drawStart;
+
+		while (wall_y < drawend)
+		{
+			my_mlx_pixel_put(&vars->img, num_rey - 1 ,wall_y, 0xDC143C);
+			wall_y++;
+		}
 		if ((int)(xs + len_sp * pow(-1,vars->sprite[i].offset_mode_cord_sprite_x)) == (int)xs
 		&& (int)(ys + len_sp * pow(-1,vars->sprite[i].offset_mode_cord_sprite_x)) == (int)ys
 		&& vars->sprite[i].len_sprt >= 0 )
 		{
-
 		}
 		i++;
 	}
-//
-//	xs = x + (len_to_sprt - lenx) * pow(-1,vars->sprite[i].offset_mode_cord_sprite_x);
-//	ys = y + (len_to_sprt - lenx) * pow(-1,vars->sprite[i].offset_mode_cord_sprite_y);
-
 }
 
 
@@ -261,9 +268,9 @@ void rey(t_vars* vars)
 	double cyx;
 
 
-	double angle_offset = ANLGLE / N_REY;
-	double max_angle = vars->angle_p + ANLGLE / 2.0;
-	double min_angle = vars->angle_p - ANLGLE / 2.0;
+	double angle_offset = ANLGLe / N_ReY;
+	double max_angle = vars->angle_p + ANLGLe / 2.0;
+	double min_angle = vars->angle_p - ANLGLe / 2.0;
 
 	int num_rey = 1;
 
@@ -278,17 +285,14 @@ void rey(t_vars* vars)
 	double x;
 
 
-
-	vars->no_so = -1;
-	vars->we_ea = -1;
-
-
+	int i;
 	while (min_angle <= max_angle)
 	{
-		int i = 0;
+		i = -1;
 		while (i++ < 100)
 		{
 			vars->sprite[i].create = -1;
+			vars->sprite[i].len_sprt = 1;
 		}
 		vars->no_so = (sin(min_angle) > 0) ?  1 : 0;
 		vars->we_ea = (cos(min_angle) < 0) ?  1 : 0;
@@ -310,10 +314,8 @@ void rey(t_vars* vars)
 				vars->sprite[n_sprt].enter_on_lenx_y = y;
 				vars->sprite[n_sprt].create = 1;
 				n_sprt++;
-				printf("1\n");
-
 			}
-			if (y < mapWidth && y > 0 && worldMap[(int)cx - vars->we_ea][(int)y] != 0)
+			if (y < mapWidth && y > 0 && worldMap[(int)cx - vars->we_ea][(int)y] == 1)
 			{
 				lenx = len;
 				break;
@@ -325,16 +327,14 @@ void rey(t_vars* vars)
 			ft_round(min_angle, &cyx, &cy, vars);
 			len = fabs((vars->y - cy) / (sin(min_angle) + 0.000002));
 			x = vars->x + (len * cos(min_angle));
-			if (x < mapWidth && x > 0 && worldMap[(int)x][(int)cy - vars->no_so]== 9)
+			if (x < mapWidth && x > 0 && worldMap[(int)x][(int)cy - vars->no_so] == 9)
 			{
 				vars->sprite[n_sprt].enter_on_leny_x = x;
 				vars->sprite[n_sprt].enter_on_leny_y = cy;
 				vars->sprite[n_sprt].create = 1;
 				n_sprt++;
-				printf("2\n");
-
 			}
-			if (x < mapWidth && x > 0 && (worldMap[(int)x][(int)cy - vars->no_so] != 0))
+			if (x < mapWidth && x > 0 && (worldMap[(int)x][(int)cy - vars->no_so] ==1))
 			{
 				leny = len;
 				break;
@@ -350,18 +350,24 @@ void rey(t_vars* vars)
 		}
 		else
 		{
-			vars->color = (vars->no_so) ? 0x009400D3 : 0x002E8B57;
+			vars->color = (vars->no_so) ? 0x009400D3 : 0x002e8B57;
 			len = leny;
 			vars->no_so = -1;
 		}
 
+		i = -1;
+		while (i++ < 100)
+		{
+		}
+
 
 		ft_render(vars, len, min_angle, num_rey);
-		ft_render_sprite((lenx + leny) / 2 , lenx, cx, y, vars);
+		ft_render_sprite((lenx + leny) / 2 , lenx, cx, y, vars, num_rey);
 
 		num_rey++;
 		min_angle += angle_offset;
 	}
+
 	mlx_put_image_to_window(vars->mlx, vars->win, vars->img.img, 0, 0);
 }
 
@@ -379,6 +385,8 @@ int             main(void)
 	while (i++ < 100)
 	{
 		vars.sprite[i].create = -1;
+		//vars.sprite[i].len_sprt = 1;
+
 	}
 
 
